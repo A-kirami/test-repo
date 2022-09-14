a1b6a1236364578536d64f01cc700bd6aba446ed
Akirami <839592615@qq.com>
2022-09-15 00:30:41 +0800

    ...

--- test_repo/__init__.py
+++ test_repo/__init__.py
@@ -1,22 +1,10 @@
 class A:
-    def __init__(self) -> None:
-        pass
-
-    def __add__(self, other):
-        raise NotImplementedError
-
-    def __test__(self):
-        ...
+    def __init__(self, text: str) -> None:
+        self.text = text
 
     def test(self):
-        ...
+        return self.text
 
 
 def add(a: int, b: int) -> int:
     return a + b
-
-
-def forloop() -> None:
-    for i in range(10):
-        if i > 5:
-            return True
